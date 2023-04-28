24a380a4f0f25ec54186a9e1e643338b9b8eb7ce
siranovel <siranovel@gmail.com>
2023-04-28 11:57:55 +0900

    Add files via upload

--- ext/num4inte/CNum4Inte.c
+++ ext/num4inte/CNum4Inte.c
@@ -3,13 +3,19 @@
 #include <assert.h>
 #include "CNum4Inte.h"
 
-static double CNum4Inte_doLeftReimannSumMetod(double a, double b, double h, Func func);
+static double CNum4Inte_doLeftReimannSumMethod(double a, double b, double h, Func func);
 static double CNum4Inte_doRigtReimannSumMetod(double a, double b, double h, Func func);
-static double CNum4Inte_doNewtonMethod(double a, double b, double h, Func func);
+static double CNum4Inte_doTrapezioidalRule(double a, double b, double h, Func func);
+static double CNum4Inte_doSimpsonRule(double a, double b, double h, Func func);
 static CNum4Inte _cNum4Inte = {
-    .FP_leftReimannSumMetod = CNum4Inte_doLeftReimannSumMetod,
-    .FP_rigtReimannSumMetod = CNum4Inte_doRigtReimannSumMetod,
-    .FP_newtonMethod        = CNum4Inte_doNewtonMethod,
+    .reimann = {
+        .FP_leftReimannSumMethod = CNum4Inte_doLeftReimannSumMethod,
+        .FP_rigtReimannSumMethod = CNum4Inte_doRigtReimannSumMetod,
+    },
+    .newton = {
+        .FP_trapezioidalRule     = CNum4Inte_doTrapezioidalRule,
+        .FP_simpsonRule          = CNum4Inte_doSimpsonRule,
+    },
 };
 /**************************************/
 /* InterFface部                       */
@@ -17,23 +23,33 @@ static CNum4Inte _cNum4Inte = {
 /**************************************/
 /* Class部                            */
 /**************************************/
-double CNum4Inte_leftReimannSumMetod(double a, double b, double h, Func func)
+double CNum4Inte_reimann_leftReimannSumMethod(double a, double b, double h, Func func)
 {
     assert(func != 0);
+    assert(a < b);
 
-    return _cNum4Inte.FP_leftReimannSumMetod(a, b, h, func);
+    return _cNum4Inte.reimann.FP_leftReimannSumMethod(a, b, h, func);
 }
-double CNum4Inte_rigtReimannSumMetod(double a, double b, double h, Func func)
+double CNum4Inte_reimann_rigtReimannSumMethod(double a, double b, double h, Func func)
 {
     assert(func != 0);
+    assert(a < b);
 
-    return _cNum4Inte.FP_rigtReimannSumMetod(a, b, h, func);
+    return _cNum4Inte.reimann.FP_rigtReimannSumMethod(a, b, h, func);
 }
-double CNum4Inte_newtonMethod(double a, double b, double h, Func func)
+double CNum4Inte_rewton_trapezioidalRule(double a, double b, double h, Func func)
 {
     assert(func != 0);
+    assert(a < b);
 
-    return _cNum4Inte.FP_newtonMethod(a, b, h, func);
+    return _cNum4Inte.newton.FP_trapezioidalRule(a, b, h, func);
+}
+double CNum4Inte_rewton_simpsonRule(double a, double b, double h, Func func)
+{
+    assert(func != 0);
+    assert(a < b);
+
+    return _cNum4Inte.newton.FP_simpsonRule(a, b, h, func);
 }
 /**************************************/
 /* 処理実行部                          */
@@ -41,10 +57,10 @@ double CNum4Inte_newtonMethod(double a, double b, double h, Func func)
 /*
  * 左リーマン和法
  */
-static double CNum4Inte_doLeftReimannSumMetod(double a, double b, double h, Func func)
+static double CNum4Inte_doLeftReimannSumMethod(double a, double b, double h, Func func)
 {
     double rimann = 0.0;
-    double x = 0.0;
+    double x;
 
     for (x = a; x < b; x += h) {
         rimann += func(x);
@@ -57,7 +73,7 @@ static double CNum4Inte_doLeftReimannSumMetod(double a, double b, double h, Func
 static double CNum4Inte_doRigtReimannSumMetod(double a, double b, double h, Func func)
 {
     double rimann = 0.0;
-    double x = 0.0;
+    double x;
 
     for (x = a; x < b; x += h) {
         rimann += func(x + h);
@@ -65,21 +81,42 @@ static double CNum4Inte_doRigtReimannSumMetod(double a, double b, double h, Func
     return rimann * h;
 }
 /*
- * ニュートン・シンプソン法
+ * 台形公式
  */
-static double CNum4Inte_doNewtonMethod(double a, double b, double h, Func func)
+static double CNum4Inte_doTrapezioidalRule(double a, double b, double h, Func func)
 {
-    double newton = 0.0;
-    double x = 0.0;
+    double trapezoidal = 0.0;
     double fx = func(a);
     double fxh = 0.0;
+    double x;
 
     for (x = a; x < b; x += h) {
         fxh = func(x + h);
-        newton += (fx + fxh) /2;
+        trapezoidal += (fx + fxh);
         fx = fxh;
     }    
-    return newton * h;
+    return trapezoidal * h / 2.0;
+}
+/*
+ * シンプソンの公式
+ */
+static double CNum4Inte_doSimpsonRule(double a, double b, double h, Func func)
+{
+    double simpson = 0.0;
+    double fx = func(a);
+    double fxh = 0.0;
+    double h2 = h / 2.0;
+    double x;
+
+    for (x = a; x < b; x += h) {
+        double fxh2 = func(x + h2);
+        double fxh  = func(x + h);
+
+        simpson += (fx + fxh + 4 * fxh2);
+        fx = fxh2;
+    }
+    return simpson * h / 6.0;
+
 }
 
 
