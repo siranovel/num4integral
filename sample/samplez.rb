8f786d605fed750e2b7accfceebce61ca540c3a8
siranovel <siranovel@gmail.com>
2023-04-28 11:58:50 +0900

    Add files via upload

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -8,30 +8,38 @@ class Num4InteTest
             next 4 / (1 + x * x)
         }
     end
-    def leftReimannSumMetodTest
-        y = Num4InteLib.leftReimannSumMetod(0, 1, @h, @func)
+    def leftReimannSumMethodTest
+        y = Num4InteLib.leftReimannSumMethod(0, 1, @h, @func)
         print "PI:", @pi
         print " "
         print y                        # 3.1425924869231237
         puts
     end
-    def rigtReimannSumMetodTest
-        y = Num4InteLib.rigtReimannSumMetod(0, 1, @h, @func)
+    def rigtReimannSumMethodTest
+        y = Num4InteLib.rigtReimannSumMethod(0, 1, @h, @func)
         print "PI:", @pi
         print " "
         print y                        # 3.140592486923123
         puts
     end
-    def newtonMethodTest
-        y = Num4InteLib.newtonMethod(0, 1, @h, @func)
+    def trapezioidalRuleTest
+        y = Num4InteLib.trapezioidalRule(0, 1, @h, @func)
         print "PI:", @pi
         print " "
-        print y                        # 3.140592486923123
+        print y                        # 3.141592486923125
+        puts
+    end
+    def simpsonRuleTest
+        y = Num4InteLib.simpsonRule(0, 1, @h, @func)
+        print "PI:", @pi
+        print " "
+        print y                        # 3.1417591952148007
         puts
     end
 end
 tst = Num4InteTest.new
-tst.leftReimannSumMetodTest()
-tst.rigtReimannSumMetodTest()
-tst.newtonMethodTest()
+tst.leftReimannSumMethodTest()
+tst.rigtReimannSumMethodTest()
+tst.trapezioidalRuleTest()
+tst.simpsonRuleTest()
 
